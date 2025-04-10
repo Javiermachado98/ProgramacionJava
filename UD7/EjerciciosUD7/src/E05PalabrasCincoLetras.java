bb971acdbd443f76534de76fa4a1cbc0841ada7b
Javier Machado WIN <javier-0198@hotmail.com>
2025-04-10 23:00:55 +0200

    ejercicio 5 resuelto

--- UD7/EjerciciosUD7/src/E05PalabrasCincoLetras.java
+++ UD7/EjerciciosUD7/src/E05PalabrasCincoLetras.java
@@ -5,38 +5,47 @@ public class E05PalabrasCincoLetras {
 
     public static void main(String[] args) throws IOException {
 
-
-
         BufferedReader streamIn = null;
         BufferedWriter streamOut = null;
+        BufferedReader streamIn2 = null;
 
         try {
-
-            streamIn = new BufferedReader(new FileReader("src" + File.separator + "dics" + File.separator + "a.txt"));
             streamOut = new BufferedWriter(new FileWriter("src" + File.separator + "files" + File.separator + "salida.txt"));
 
-            String word;
-            while ((word = streamIn.readLine()) != null){
-                String [] words = word.split("");
+            for (char words = 'a'; words <= 'z'; words++) {
+                streamIn = new BufferedReader(new FileReader("src" + File.separator + "dics" + File.separator + words + ".txt"));
+                streamIn2 = new BufferedReader(new FileReader("src" + File.separator + "dics" + File.separator + "ñ.txt"));
 
-                if (words.length == 5){
-                    System.out.println(words.length + "caracteres");
-                    System.out.println(Arrays.toString(words));
-                    streamOut.write(Arrays.toString(words));
-                    streamOut.newLine();
+                String word;
+                String wordEnie;
+                while ((word = streamIn.readLine()) != null) {
+
+                    if (word.length() == 5) {
+
+                        streamOut.write(word);
+                        streamOut.newLine();
+
+                    }
                 }
+                while ((wordEnie = streamIn2.readLine()) != null) {
 
+                    if (wordEnie.length() == 5) {
 
+                        streamOut.write(wordEnie);
+                        streamOut.newLine();
+
+                    }
+                }
             }
-
-
-
 
         } catch (Exception e) {
             System.out.println(e);
-        }finally {
+        } finally {
             if (streamIn != null) {
                 streamIn.close();
+            }
+            if (streamOut != null) {
+                streamOut.close();
             }
             if (streamOut != null) {
                 streamOut.close();
